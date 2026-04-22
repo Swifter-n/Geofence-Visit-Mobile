3c6bd6fc5fc09de83fe9b72f73856f2b344781e2
Ibnu Saputra <ipankthedoors@gmail.com>
2026-04-22 18:52:13 +0700

    update

--- lib/data/repositories/master_data_repository.dart
+++ lib/data/repositories/master_data_repository.dart
@@ -12,12 +12,18 @@ class MasterDataRepository {
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
       final decoded = jsonDecode(response.body);
-
       final List jsonList = decoded['data'] ?? [];
-
       return jsonList.map((e) => MasterRouteModel.fromJson(e)).toList();
     } else {
-      throw Exception('Gagal mengambil data rute: ${response.statusCode}');
+      String errorMessage = 'Gagal mengambil data rute: ${response.statusCode}';
+      try {
+        final decodedError = jsonDecode(response.body);
+        if (decodedError['message'] != null) {
+          errorMessage = decodedError['message'];
+        }
+      } catch (_) {} // Abaikan jika body bukan JSON
+
+      throw Exception(errorMessage);
     }
   }
 }

00979ce3ba9aa343354d9a2773166036c2149ab6
Ibnu Saputra <54946777+Swifter-n@users.noreply.github.com>
2026-06-28 01:00:22 +0700

    Update variables.dart

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -2,8 +2,4 @@ class Variables {
   // static const String appName = "Geofence Visit Mobile";
   // static const String baseUrl = "http://localhost:8000/api/";
   // static const String imageBaseUrl = "http://localhost:8000/storage/";
-
-  static const String appName = "Geofence Visit Mobile";
-  static const String baseUrl = "http://192.168.1.10:8000/api/";
-  static const String imageBaseUrl = "http://192.168.1.10:8000/storage/";
 }
